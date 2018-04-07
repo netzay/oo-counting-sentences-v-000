024976dd3f8d6cd8a8e3cdfeb340ca897cfbee24
Ally Netzel <netzelally@gmail.com>
2018-04-07 20:52:22 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,7 @@ class String
   end
 
   def count_sentences
-    self.split(/\?|\.|!/). map
+    self.split(/\?|\.|!/).map {|word| word.capitalize }.join(" ")}: #{supply.join(", ")}" 
     #binding.pry
   end
 end
